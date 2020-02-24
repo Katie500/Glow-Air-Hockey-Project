04ebe610b623a5817b3dabd6d1d656f7f3406bd9
GabrahamLincoln <58123771+GabrahamLincoln@users.noreply.github.com>
2020-02-23 18:10:53 -0700

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,55 +1,31 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Table {
-	private double width = 600;
-	private double height = 800;
+	final double WIDTH = 600;
+	final double HEIGHT = 800;
+	final double CENTER_X = WIDTH / 2;
+	final double CENTER_Y = HEIGHT / 2;
 	
-	final double CENTER_X = width / 2;
-	final double CENTER_Y = height / 2;
+	private Paddle player_one;
+	private Paddle player_two;
 	
-	private ArrayList<Paddle> paddles = new ArrayList<Paddle>(2);
-	
-	public double getWidth() {
-		return new Table().width;
-	}
-	
-	public double getHeight() {
-		return new Table().height;
-	}
-	
-	public double getCenterX() {
-		return new Table().CENTER_X;
-	}
-	
-	public double getCenterY() {
-		return new Table().CENTER_Y;
+	public Table(Paddle player_one, Paddle player_two) {
+		this.player_one = player_one;
+		this.player_two = player_two;
 	}
 	
 	public Paddle getPlayerOne() {
-		ArrayList<Paddle> temp = new ArrayList<Paddle>(Arrays.asList(paddles.get(0), paddles.get(1)));
-		return temp.get(0);
+		return new Table(player_one, player_two).player_one;
 	}
 	
 	public Paddle getPlayerTwo() {
-		ArrayList<Paddle> temp = new ArrayList<Paddle>(Arrays.asList(paddles.get(0), paddles.get(1)));
-		return temp.get(1);
+		return new Table(player_one, player_two).player_two;
 	}
-	
-	public void setWidth(double width) {
-		this.width = width;
-	}
-	
-	public void setHeight(double height) {
-		this.height = height;
-	}
-	
+
 	public void setPlayerOne(Paddle paddle) {
-		paddles.set(0,  paddle);
+		player_one = paddle;
 	}
 	
 	public void setPlayerTwo(Paddle paddle) {
-		paddles.set(1,  paddle);
+		player_two = paddle;
 	}
 }
