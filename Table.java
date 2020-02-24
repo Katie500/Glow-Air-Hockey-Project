dc201c4837a3d734e3ff16762ab995c18b65b210
GabrahamLincoln <58123771+GabrahamLincoln@users.noreply.github.com>
2020-02-23 18:00:47 -0700

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Table {
 	private double width = 600;
@@ -8,7 +9,7 @@ public class Table {
 	final double CENTER_X = width / 2;
 	final double CENTER_Y = height / 2;
 	
-	private ArrayList<Double> paddles = new ArrayList<Double>();
+	private ArrayList<Paddle> paddles = new ArrayList<Paddle>(2);
 	
 	public double getWidth() {
 		return new Table().width;
@@ -26,11 +27,29 @@ public class Table {
 		return new Table().CENTER_Y;
 	}
 	
+	public Paddle getPlayerOne() {
+		ArrayList<Paddle> temp = new ArrayList<Paddle>(Arrays.asList(paddles.get(0), paddles.get(1)));
+		return temp.get(0);
+	}
+	
+	public Paddle getPlayerTwo() {
+		ArrayList<Paddle> temp = new ArrayList<Paddle>(Arrays.asList(paddles.get(0), paddles.get(1)));
+		return temp.get(1);
+	}
+	
 	public void setWidth(double width) {
 		this.width = width;
 	}
 	
 	public void setHeight(double height) {
 		this.height = height;
+	}
+	
+	public void setPlayerOne(Paddle paddle) {
+		paddles.set(0,  paddle);
+	}
+	
+	public void setPlayerTwo(Paddle paddle) {
+		paddles.set(1,  paddle);
 	}
 }
