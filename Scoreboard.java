680c7667874440d6ad66615a5c3c75299ca83dcb
sharansarthak <59523742+sharansarthak@users.noreply.github.com>
2020-02-25 17:43:41 -0700

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -6,25 +6,25 @@ public class Scoreboard {
 	final int SCORE_TO_WIN = 7;
 	
 	public Scoreboard(int player_one_score, int player_two_score) {
-		this.player_one_score = player_one_score;
-		this.player_two_score = player_two_score;
+		this.player_one_score = player_one_score ;
+		this.player_two_score = player_two_score ; 
 	}
 	
 	public void playerOneGoal() {
-		player_one_score += 1;
-		System.out.println("Player one has scored!");
+		player_one_score += 1 ; 
+		System.out.println("Player one has scored!") ;
 	}
 	
 	public void playerTwoGoal() {
-		player_two_score += 1;
-		System.out.println("Player two has scored!");
+		player_two_score += 1 ;
+		System.out.println("Player two has scored!") ;
 	}
 	
 	public int getPlayerOneScore() {
-		return new Scoreboard(player_one_score, player_two_score).player_one_score;
+		return new Scoreboard(player_one_score, player_two_score).player_one_score ;
 	}
 	
 	public int getPlayerTwoScore() {
-		return new Scoreboard(player_one_score, player_two_score).player_two_score;
+		return new Scoreboard(player_one_score, player_two_score).player_two_score ;
 	}
 }
