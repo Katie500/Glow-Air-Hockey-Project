074a9106064fe552be3f45ed6652914eb4aa7c2f
sharansarthak <59523742+sharansarthak@users.noreply.github.com>
2020-03-11 23:13:06 -0600

    Update Puck.java

--- Puck.java
+++ Puck.java
@@ -1,23 +1,35 @@
 
-public class Puck {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+
+public class Puck extends Circle {
 	private String colour;
 	private double x;
 	private double y;
-	private double velocity_x = 200;
-	private double velocity_y = 200;
-	
-	final double PUCK_SIZE = 20; // The radius of the puck.
+	private double velocity_x = 500;
+	private double velocity_y = 500;
+	final double SIZE = 20; // The radius of the puck.
 	final double MASS = 10;
-	final double MAX_SPEED = 50;
-	final double FRICTION_X = 0.997;
-	final double FRICTION_Y = 0.997;
-	final double ACCELERATION = 1;
+	final double MAX_SPEED = 1000;
+	final double FRICTION = 0.997;
+
 	
 	// Constructor used to create a puck.
 	public Puck(String colour) {
-		this.colour = colour;
+		this.colour = colour ;
+		setRadius(SIZE) ;
+		setFill(Color.valueOf(colour));
+
 	}
-	
+	public Puck(Puck puck){
+		this.colour = puck.colour ;
+		this.x = puck.x ;
+		this.y = puck.y ;
+		this.velocity_x = puck.velocity_x ;
+		this.velocity_y = puck.velocity_y ;
+	}
+
 	// This constructor will be used by certain getter methods to eliminate privacy leaks while retaining the necessary values.
 	public Puck(double x, double y, double velocity_x, double velocity_y) {
 		this.x = x;
@@ -33,22 +45,23 @@ public class Puck {
 	
 	// Getter for the puck's x velocity.
 	public double getVelocityX() {
-		return new Puck(x, y, velocity_x, velocity_y).velocity_x;
+		return new Puck(this).velocity_x;
 	}
 	
 	// Getter for the puck's y velocity.
 	public double getVelocityY() {
-		return new Puck(x, y, velocity_x, velocity_y).velocity_y;
+		return new Puck(this).velocity_y;
 	}
 	
 	// Getter for the puck's x position.
 	public double getX() {
-		return new Puck(x, y, velocity_x, velocity_y).x;
+		return new Puck(this).x;
+
 	}
 	
 	// Getter for the puck's y position.
 	public double getY() {
-		return new Puck(x, y, velocity_x, velocity_y).y;
+		return new Puck(this).y;
 	}
 	
 	// Setter for the puck's colour.
@@ -58,21 +71,29 @@ public class Puck {
 	
 	// Setter for the puck's x velocity.
 	public void setVelocityX(double velocity) {
-		this.velocity_x = velocity;
+		if(velocity_x <= MAX_SPEED)
+			this.velocity_x = velocity ;
+		else
+			this.velocity_x = MAX_SPEED ;
 	}
 	
 	// Setter for the puck's y velocity.
 	public void setVelocityY(double velocity) {
-		this.velocity_y = velocity;
+		if(velocity_x <= MAX_SPEED)
+			this.velocity_x = velocity ;
+		else
+			this.velocity_x = MAX_SPEED ;
 	}
 	
 	// Setter for the puck's x position.
 	public void setX(double x) {
 		this.x = x;
+		setCenterX(x);
 	}
 	
 	// Setter for the puck's y position.
 	public void setY(double y) {
 		this.y = y;
+		setCenterY(y);
 	}
 }
