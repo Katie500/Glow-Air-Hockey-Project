88775ae813b683c14372ac7f725d06dda8dc71f0
taylor-massey <60122575+taylor-massey@users.noreply.github.com>
2020-03-29 22:44:19 -0600

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,40 +1,40 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javafx.scene.effect.DropShadow;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import javafx.scene.shape.Rectangle;
-
-public class MainMenu extends JPanel implements MouseListener{
-    protected static int width = 600;
+public class MainMenu extends JPanel  {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected static int width = 600;
     protected static int height = 800;
+    
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-        //button boxes
-        g.setColor(Color.gray);
+        // Button boxes
+        g.setColor(Color.GRAY);
         g.fillRect(width/2-200, height/5, 400, 60);
         g.fillRect(width/2-50, height/5*2, 100, 60);
         g.fillRect(width/2-50, height/5*3, 100, 60);
         g.fillRect(width/2-50, height/5*4, 100, 60);
         
-        //borders
-        g.setColor(Color.red);
+        // Borders
+        g.setColor(Color.RED);
         g.fillRect(0, 0, 2, 800);
         g.fillRect(width-18, 0, 2, 800);
         g.fillRect(0, 0, 600, 2);
         g.fillRect(0, height-41, 600, 2);
         
-        
-        
-        
-        //labels
-        g.setColor(Color.red);
+        // Labels
+        g.setColor(Color.RED);
         g.setFont(new Font("Bauhaus 93", Font.PLAIN, 45));
         g.drawString("Glow Air Hockey", width/2-160, height/5 + 45);
         g.drawString("Play", width/2 - 45, height/5*2+42);
@@ -52,59 +52,9 @@ public class MainMenu extends JPanel implements MouseListener{
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(menu);
-        
+        frame.setResizable(false);
+        frame.addMouseListener(new MouseClick());
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        int mx = e.getX();
-        int my = e.getY();
-
-        
-        //playbutton
-        if (mx >= width/2-50 && mx <= width/2+50) {
-            if(my >= height/5*2 && my <= height/5*2+60) {
-                Main.state = Main.STATE.GAME;
-            }
-        }
-        
-        //infobutton
-        if (mx >= width/2-50 && mx <= width/2+50) {
-            if(my >= height/5*3 && my <= height/5*3+60) {
-                //display instructions and names class etc;
-            }
-        }
-        
-        //quitbutton
-        if (mx >= width/2-50 && mx <= width/2+50) {
-            if(my >= height/5*4 && my <= height/5*4+60) {
-                System.exit(0);
-            }
-        }        
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
     
-        // TODO Auto-generated method stub
-        
-    }
 }
