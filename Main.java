2ef7abed4aaa1ec3ff7b42c7764c8d5cf5cd4c1a
taylor-massey <60122575+taylor-massey@users.noreply.github.com>
2020-04-13 23:24:56 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,7 +33,7 @@ import javafx.util.Duration;
 
 
 
-
+//setting the main class as an extension of the Application class
 public class Main extends Application {
 	protected static Table table;
 	protected static boolean up, down, left, right, w, a, s, d;
@@ -69,27 +69,30 @@ public class Main extends Application {
 	    GAME,
 	    OVER
 	}
+	//setting the first game state to menu
 	public static state game_state = state.MENU;
 	
 	public static void main(String [] args) {        //Pretty sure this is not necessary 
 		launch(args);
 	}
 	
+	//setting up the start menu
 	@Override
 	public void start(Stage primaryStage) {
 		createTable();
 		createLabels();
 		GameScreen.createBorderGlows();
 		
+		//setting the title, size, and background for the primary stage
 		primaryStage.setTitle("Glow Air Hockey");
 		layout.setPrefSize(Rink.WIDTH, Rink.HEIGHT);
 		layout.setStyle("-fx-background-color: BLACK;");
 //		Rink.setScreen();
 		rink.runRink();
 		rink_screen = new Scene(rink);
-				// Timeline to call the event handler every 10ms to update the table.
+				
 		
-		
+		// Timeline to call the event handler every 10ms to update the table.
 		timeline = new Timeline(new KeyFrame(Duration.millis(DELAY), new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -106,12 +109,15 @@ public class Main extends Application {
 					}
 				}
 				
+				//checking the game state to see if it is the state of the game
 				else if (Main.game_state == state.GAME) {
 					boolean goal = false;
 					ArrayList<Boolean> goal_data;
 					
+					//removes the score from the screen after the player has scored
         			removeScore();
         			
+        			//if the player pauses the game, the label shows that it is paused
         			if (paused) {
         				if (!text_display.getChildren().contains(paused_label)) {
         					text_display.getChildren().add(paused_label);
@@ -143,31 +149,42 @@ public class Main extends Application {
 	        			Controller.controllerOne();
 	        			Controller.controllerTwo();
 	        			
+	        			
+	        			//if the game is over, change the main game state
 	        			if (table.gameOver()) {
 	        				Main.game_state = state.OVER;
 	        				
+	        				
+	        				//we check if player one won and if so, we send a message to the screen
+                            //that says who one and set winner equal to player one
 							if (table.getPlayerOne().getScore() == table.getPlayerOne().SCORE_TO_WIN) {
 								p1.setText("Game Over\n" + table.getPlayerOne().getName() + "\nWon!");
 								winner = p1;
 							}
 							
+							//if player one didn't win, we sent a message to the screen saying that player 
+                            //two won and set winner equal to player two
 							else {
 								p2.setText("Game Over\n" + table.getPlayerTwo().getName() + "\nWon!");
 								winner = p2;
 							}
 							
+							//getting the scores for both players
 							p1_score.setText(table.getPlayerOne().getScore() + "");
 							p2_score.setText(table.getPlayerTwo().getScore() + "");
 							
+							//displaying the winner on the screen
 							text_display.setSpacing(table.HEIGHT / 10);
 							text_display.getChildren().addAll(p1_score, winner, p2_score);
 							layout.getChildren().add(text_display);
 							
+							//asking if user wants to play again after the game over message has been displayed for a while. 
 							PauseTransition pause = new PauseTransition(Duration.seconds(5));
 	        				pause.setOnFinished(e -> playAgain());
 	        				pause.play();
 	        			}
 	        			
+	        			//if new goal, show scores 
 	        			else if (goal) {
 	        				displayScore(goal_data.get(0), goal_data.get(1));
 	        				PauseTransition pause = new PauseTransition(Duration.seconds(2));
@@ -178,6 +195,7 @@ public class Main extends Application {
         			}
 				}
 				
+				//setting up for new game
 				else {
 					if (new_game) {
 						reset();
@@ -189,28 +207,28 @@ public class Main extends Application {
 		}));
 				
 		
-		
+		//getting the elements needed for the game
 		rink.getChildren().addAll(table.getPuck(), table.getPlayerOne(), table.getPlayerTwo(), table.getPlayerOne().getCenterCircle(), table.getPlayerTwo().getCenterCircle());
-		//scene = new Scene(layout);
-		//Controller.setControls();
-		menu_screen = new Scene(menu);
 		
+		//setting menu screen
+		menu_screen = new Scene(menu);
+		//setting timeline
 		timeline.setCycleCount(Timeline.INDEFINITE);
 		timeline.setAutoReverse(true);
 		timeline.play();
-		
+		//running menu
 		menu.runMenu();
-		
+		//setting primary stage if game state is menu
 		if (game_state == state.MENU) {
 			primaryStage.setScene(menu_screen);
 
 		}
-		
+		//setting size for primary stage
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 	
-	
+	//creating the table for the game
 	public void createTable() {
 		String player_one_name = "";
 		String player_two_name = "";
@@ -228,8 +246,8 @@ public class Main extends Application {
 		table.getPlayerTwo().setColour(table.getPlayerTwo().getColour());
 	}
 	
-	
-	public ArrayList<Boolean> updateGame() {                    // This function updates what is necessary with each tick in the timeline.
+	// This function updates what is necessary with each tick in the timeline.
+	public ArrayList<Boolean> updateGame() {                    
 		table.applyFriction(DELAY);
 		table.getPuck().updatePuckPosition(DELAY);
 		table.updatePaddlePositions(DELAY);
@@ -243,7 +261,7 @@ public class Main extends Application {
 	
 	
 
-	
+	//creating elements for score, game over and play again button.
 	public void createLabels() {
 		int font_size = 60;
 		Font f = new Font(Font.getDefault().getStyle(), font_size);
@@ -265,10 +283,11 @@ public class Main extends Application {
 				f = new Font(font_name, font_size * 1.3);
 			}
 		}
-		
+		//setting font of scores that show up when a player scores or the game is over. 
 		p1_score.setFont(f);
 		p2_score.setFont(f);
 		
+		//setting up pause screen text
 		paused_label.setText("Paused");
 		paused_label.setFont(getFont());
 		paused_label.setAlignment(Pos.CENTER);
@@ -276,17 +295,20 @@ public class Main extends Application {
 		paused_label.setTextFill(Color.CHARTREUSE);
 		paused_label.setEffect(green_border_glow);
 		
+		//setting up game over text
 		game_over_label.setText("Game Over");
 		game_over_label.setFont(getFont());
 		game_over_label.setTextFill(Color.CHARTREUSE);
 		game_over_label.setEffect(green_border_glow);
 		
+		//setting up game winner message
 		winner.setFont(getFont());
 		winner.setTextFill(Color.CHARTREUSE);
 		winner.setAlignment(Pos.CENTER);
 		winner.setTextAlignment(TextAlignment.CENTER);
 		winner.setEffect(green_border_glow);
 		
+		//setting the player one scored font
 		p1.setText(table.getPlayerOne().getName() + "\nScored!");
 		p1.setFont(getFont());
 		p1.setAlignment(Pos.CENTER);
@@ -295,6 +317,7 @@ public class Main extends Application {
 		p1.setEffect(green_border_glow);
 		p1.setPrefSize(table.WIDTH, table.HEIGHT / 2);
 		
+		//setting the player two scored font
 		p2.setText(table.getPlayerTwo().getName() + "\nScored!");
 		p2.setFont(getFont());
 		p2.setAlignment(Pos.CENTER);
@@ -303,12 +326,14 @@ public class Main extends Application {
 		p2.setEffect(green_border_glow);
 		p2.setPrefSize(table.WIDTH,  table.HEIGHT / 2);
 		
+		//setting table display
 		text_display.setPrefSize(table.WIDTH,  table.HEIGHT);
 		text_display.setAlignment(Pos.CENTER);
 		text_display.setLayoutX(0);
 		text_display.setLayoutY(0);
 		text_display.setSpacing(table.HEIGHT / 10);
 		
+		//setting the new game text
 		play_again.setText("New Game?");
 		play_again.setTextFill(Color.CHARTREUSE);
 		play_again.setFont(getFont());
@@ -317,10 +342,12 @@ public class Main extends Application {
 		play_again.setTextAlignment(TextAlignment.CENTER);
 	}
 	
+	//method to display the score
 	public void displayScore(boolean p1_goal, boolean p2_goal) {	
 		p1_score.setText(Integer.toString(table.getPlayerOne().getScore()));
 		p2_score.setText(Integer.toString(table.getPlayerTwo().getScore()));
 		
+		// if a certain player scores display their name and both new scores
 		if (p1_goal) {
 			text_display.getChildren().addAll(p1_score, p1, p2_score);
 			rink.getChildren().add(text_display);
@@ -340,6 +367,7 @@ public class Main extends Application {
 		rink.getChildren().add(text_display);
 	}
 	
+	//clearing the score from the the display
 	public void removeScore() {
 		text_display.getChildren().clear();
 		
@@ -366,10 +394,12 @@ public class Main extends Application {
 		return font;
 	}
 	
+	//building button displayed after game over to play again. 
 	public void playAgain() {
 		timeline.pause();
 		play_again.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
+			//handling of play again button click 
+		    @Override
 			public void handle(ActionEvent e) {
 				new_game = true;
 				timeline.play();
@@ -377,6 +407,7 @@ public class Main extends Application {
 				reset();
 			}
 		});
+		//clearing the display to play again
 		text_display.getChildren().clear();
 		text_display.getChildren().add(play_again);
 //		rink.getChildren().add(layout);		
@@ -386,6 +417,7 @@ public class Main extends Application {
 		}
 	}
 	
+	//method to reset the game and begin again from the menu
 	public void reset() {
 		text_display.getChildren().clear();
 		layout = new Pane();
