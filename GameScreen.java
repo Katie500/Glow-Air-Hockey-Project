90ed210a59349ad68bf6132c81f8b773d1c48e25
taylor-massey <60122575+taylor-massey@users.noreply.github.com>
2020-04-14 01:50:27 -0600

    Update GameScreen.java

--- GameScreen.java
+++ GameScreen.java
@@ -6,23 +6,26 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public abstract class GameScreen extends Pane {
+    //declaring values that will remain unchanged in both gui screens
     public final static int WIDTH = 600;
     public final static int HEIGHT = 800;
     final static double CENTER_X = WIDTH / 2;
     final static double CENTER_Y = HEIGHT / 2;
-    final static double GOAL_SIZE = 150;
+    public final static double GOAL_SIZE = 200;
     
     protected static DropShadow green_border_glow = new DropShadow();
     protected static DropShadow red_border_glow = new DropShadow();
     
+    //abstract method that must be used in both gui screens although differently
     public abstract void setScreen();
     
+    //method used the same way in both gui screens
     public static void createBorderGlows() {
-        green_border_glow.setColor(Color.CHARTREUSE);
-        green_border_glow.setOffsetX(0f);
-        green_border_glow.setOffsetY(0f);
-        green_border_glow.setWidth(50);
-        green_border_glow.setHeight(50);
+        getGreen_border_glow().setColor(Color.CHARTREUSE);
+        getGreen_border_glow().setOffsetX(0f);
+        getGreen_border_glow().setOffsetY(0f);
+        getGreen_border_glow().setWidth(50);
+        getGreen_border_glow().setHeight(50);
         
         red_border_glow.setColor(Color.RED);
         red_border_glow.setOffsetX(0f);
@@ -30,4 +33,17 @@ public abstract class GameScreen extends Pane {
         red_border_glow.setWidth(50);
         red_border_glow.setHeight(50);
     }
+
+    
+    
+    //Getter and setter for green border glow to access in main. 
+    public static DropShadow getGreen_border_glow() {
+        return green_border_glow;
+    }
+    public static void setGreen_border_glow(DropShadow green_border_glow) {
+        GameScreen.green_border_glow = green_border_glow;
+    }
+
+
+
 }
